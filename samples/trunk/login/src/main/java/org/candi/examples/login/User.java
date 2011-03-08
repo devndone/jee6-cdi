6f3cdfb8811c6552dbedf3ed652aa6ac79e30777
cloud.potato.salad <cloud.potato.salad@gmail.com>
2011-03-08 22:04:43 +0000

    fixed serialization issue for instances of servers that are replicating sessions

--- samples/trunk/login/src/main/java/org/candi/examples/login/User.java
+++ samples/trunk/login/src/main/java/org/candi/examples/login/User.java
@@ -1,11 +1,15 @@
 package org.candi.examples.login;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
-public class User
+public class User implements Serializable
 {
+ 	private static final long serialVersionUID = 1L;
+ 
    @Id
    private String username;
    private String name;
