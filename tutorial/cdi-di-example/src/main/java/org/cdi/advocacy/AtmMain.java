52c6b182943839fa9b547fbeb4861f5b8fe8a9f3
cloud.potato.salad <cloud.potato.salad@gmail.com>
2011-03-26 01:54:16 +0000

    changing to new resources

--- tutorial/cdi-di-example/src/main/java/org/cdi/advocacy/AtmMain.java
+++ tutorial/cdi-di-example/src/main/java/org/cdi/advocacy/AtmMain.java
@@ -2,14 +2,13 @@ package org.cdi.advocacy;
 
 import java.math.BigDecimal;
 
-import org.cdiadvocate.beancontainer.BeanContainer;
-import org.cdiadvocate.beancontainer.BeanContainerManager;
+import org.cdisource.beancontainer.BeanContainer;
+import org.cdisource.beancontainer.BeanContainerManager;
 
 
 public class AtmMain {
 
 	static BeanContainer beanContainer =  BeanContainerManager.getInstance();
-	static { beanContainer.start(); }
 
 	public static void main(String[] args) throws Exception {
 		AutomatedTellerMachine atm = (AutomatedTellerMachine) beanContainer
